236e9f2f7f32781c415fde54f809c90df4c52067
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-03-23 10:54:29 -0300

    Update vehiculos.py

--- game/vehiculos.py
+++ game/vehiculos.py
@@ -1,1 +1,17 @@
+class Vehiculos():
+    def __init__(self):
+        self.__modelo=""
+        self.__marca=""
+        print("Vehiculos..")
+        
+
+    def setMarca(self, marca):
+        self.__marca=marca
+    def getMarca(self):
+        return "{}".format(self.__marca)        
+    
+    def setModelo(self, modelo):
+        self.__modelo=modelo
+    def getModelo(self):
+        return "{}".format(self.__modelo)
 
